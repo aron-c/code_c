31487e06f1ac147c5904c488f384dc8baada36bb
aron-c <610322518@qq.com>
2022-04-14 22:10:35 +0800

    4.14

--- c++main/test3.cpp
+++ c++main/test3.cpp
@@ -1,34 +1,52 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-const double PI=3.14;
+const double PI = 3.14;
 
-class MyClass
+class Student
 {
 public:
-    MyClass();
-    MyClass(MyClass &&) = default;
-    MyClass(const MyClass &) = default;
-    MyClass &operator=(MyClass &&) = default;
-    MyClass &operator=(const MyClass &) = default;
-    ~MyClass();
+    string mName;
+    int mId;
 
-private:
-    
+    void showStud()
+    {
+        cout << "学生姓名：" << mName << endl;
+        cout << "学生学号：" << mId << endl;
+    }
+
+    void setName(string name)
+    {
+        mName = name;
+    }
 };
 
-MyClass::MyClass()
+class Person
 {
-}
+public:
+private:
+    string mName int mPwd;
 
-MyClass::~MyClass()
-{
-}
+};
 
-int main()
+int
+main()
 {
     cout << "hello world!" << endl;
-    
-    
+
+    Student s1;
+    s1.mName = "zhangsan";
+    s1.mId = 193131389;
+    s1.showStud();
+
+    Student s2;
+    s2.setName("lisi");
+    s2.mId = 123123123;
+    s2.showStud();
+
+    PersonOne p1;
+    p1.func2();
+
     return 0;
 }
---
+++ c++main/test4.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Person
+{
+private:
+    string mName;
+    int mAge;
+    string mLove;
+
+    /* data */
+public:
+    void setName(string name)
+    {
+        mName = name;
+    }
+    string getName()
+    {
+
+        return mName;
+    };
+
+    void setAge(int age)
+    {
+        if (age < 0 || age > 100)
+        {
+
+            mAge = 18;
+            cout << "输入错误了" << endl;
+            return;
+            /* code */
+        }
+
+        mAge = age;
+    };
+
+    int getAge()
+    {
+        return mAge;
+    };
+
+    int setLove(string love)
+    {
+        mLove = love;
+    };
+};
+
+int main()
+{
+    cout << "hello world!" << endl;
+
+    Person p;
+    p.setName("zhansan");
+    cout << "hello world!" << p.getName() << endl;
+    cout << "hello world!" << p.getAge() << endl;
+
+    int age;
+    cin >> age;
+    p.setAge(age);
+    cout << "hello world!" << p.getAge() << endl;
+
+    return 0;
+}
